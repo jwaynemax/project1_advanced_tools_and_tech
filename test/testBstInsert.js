37cca68b2e408ebba7cba88d60c16002b4122ad2
jm00724 <jm00724@cs6261-vm01.uwg.westga.edu>
2023-06-03 09:35:40 -0400

    all test cases for insert into node added.

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -10,10 +10,18 @@ const tree = new BST();
 const brick0 = new Brick(4, "blue");
 const brick1 = new Brick(3, "blue");
 const brick2 = new Brick(5, "blue");
+const brick3 = new Brick(2, "blue");
+const brick4 = new Brick(6, "blue");
+const brick5 = new Brick(7, "blue");
+
 
 tree.insert(brick0);
 tree.insert(brick1);
 tree.insert(brick2);
+tree.insert(brick3);
+tree.insert(brick4);
+tree.insert(brick5);
+
 
 
 describe('testBSTInsert', function () {
@@ -45,5 +53,17 @@ describe('testLeftRightNode', function () {
       assert.equal(tree.root.data, brick0.size);
       assert.equal(tree.root.left.data, brick1.size);
       assert.equal(tree.root.right.data, brick2.size);
+    });
+});
+
+describe('testNodesOnBST', function () {
+    it('nodes sorted on tree', function () {
+
+      assert.equal(tree.root.data, brick0.size);
+      assert.equal(tree.root.left.data, brick1.size);
+      assert.equal(tree.root.right.data, brick2.size);
+      assert.equal(tree.root.left.left.data, brick3.size);
+      assert.equal(tree.root.right.right.data, brick4.size);
+      assert.equal(tree.root.right.right.right.data, brick5.size);
     });
 });
