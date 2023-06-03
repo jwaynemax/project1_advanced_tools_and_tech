48bb3ec8149d99c856811ccd4c21d3525a25801b
jm00724 <jm00724@cs6261-vm01.uwg.westga.edu>
2023-06-03 11:56:39 -0400

    constructor for LegoPile and insert Brick into BST added.

--- legos.js
+++ legos.js
@@ -1,7 +1,46 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
+var bst = require('./bst');
+const BST = bst.BST;
+
 function Brick (size, color) {
   return {
     size: size,
     color: color
+  }
+}
+
+function LegoPile () {
+  this.legoColors = {
+    "Red": new BST(),
+    "Green": new BST(),
+    "Blue": new BST(),
+    "Yellow": new BST(),
+    "Black": new BST(),
+    "White": new BST()
+  }
+  this.insert = insert;
+}
+
+function insert(brick) {
+  var current = brick.color;
+  if (current == "Red") {
+    this.legoColors["Red"].insert(brick);
+    break;
+  }
+  if (current == "Green") {
+    this.legoColors["Green"].insert(brick);
+  }
+  if (current == "Blue") {
+    this.legoColors["Blue"].insert(brick);
+  }
+  if (current == "Yellow") {
+    this.legoColors["Yellow"].insert(brick);
+  }
+  if (current == "Black") {
+    this.legoColors["Black"].insert(brick);
+  }
+  if (current == "White") {
+    this.legoColors["White"].insert(brick);
   }
 }
---
+++ test/testLegoPileInsert.js
@@ -0,0 +1,4 @@
+var legoPile = new LegoPile();
+legoPile.insert(new Brick(4, "Blue"));
+
+console.log(legoPile.legoColors["Red"]);
